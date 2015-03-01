13e5e1579c301a3e40f38892dce0ebabcb9f5a13
Reverp <contact@reverbs.pw>
2015-03-01 20:53:38 +0100

    Added SettingsFragment to SettingsActivity

--- app/src/main/java/reverb/com/instagramsectiondisabler/Settings.java
+++ app/src/main/java/reverb/com/instagramsectiondisabler/Settings.java
@@ -1,16 +1,29 @@
 package reverb.com.instagramsectiondisabler;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 public class Settings extends PreferenceActivity {
 
-    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
-        addPreferencesFromResource(R.xml.settings);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
+            addPreferencesFromResource(R.xml.settings);
+        }
     }
 
 }
