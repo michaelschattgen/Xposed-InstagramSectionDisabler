4f0e2964e08920ff273f7fb8b2a9db360f43f9b2
Reverp <contact@reverbs.pw>
2015-03-01 02:29:26 +0100

    Module will now enabled check for prefs

--- app/src/main/java/reverb/com/instagramsectiondisabler/InstagramSectionDisabler.java
+++ app/src/main/java/reverb/com/instagramsectiondisabler/InstagramSectionDisabler.java
@@ -1,16 +1,28 @@
 package reverb.com.instagramsectiondisabler;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class InstagramSectionDisabler implements IXposedHookLoadPackage {
+public class InstagramSectionDisabler implements IXposedHookLoadPackage, IXposedHookZygoteInit {
+
+    public XSharedPreferences prefs;
 
     private static void log(String log) {
         XposedBridge.log("InstagramSectionDisabler: " + log);
+    }
+
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        prefs = new XSharedPreferences(InstagramSectionDisabler.class.getPackage()
+                .getName());
+        prefs.makeWorldReadable();
     }
 
     @Override
@@ -18,15 +30,19 @@ public class InstagramSectionDisabler implements IXposedHookLoadPackage {
         if (!loadPackageParam.packageName.equals("com.instagram.android"))
             return;
 
-        XposedHelpers.findAndHookMethod("com.instagram.android.fragment.bu", loadPackageParam.classLoader, "c", new XC_MethodReplacement() {
-
-            @Override
-            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                methodHookParam.setResult(null);
-                return null;
-            }
-        });
-
-
+        if(isEnabled("photos")) {
+            XposedHelpers.findAndHookMethod("com.instagram.android.fragment.bu", loadPackageParam.classLoader, "c", new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                    methodHookParam.setResult(null);
+                    return null;
+                }
+            });
+        }
     }
+
+    public boolean isEnabled(String preference) {
+        return prefs.getBoolean(preference, false);
+    }
+
 }
